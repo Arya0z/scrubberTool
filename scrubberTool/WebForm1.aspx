1a40fa3edd12cf43c2c08c2baf6b9b260ca73390
Amit jaiswar <arya0z@outlook.com>
2023-07-10 14:52:53 +0530

    Add project files.

---
+++ scrubberTool/WebForm1.aspx
@@ -0,0 +1,99 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="scrubberTool.WebForm1" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <style type="text/css">
+        #form1 {
+            height: 359px;
+        }
+    </style>
+</head>
+<body style="height: 359px">
+    <form id="form1" runat="server">
+
+
+        <div class="container-md border shadow-lg rounded-4 bg-light p-4 mt-3">
+
+            <div class=" row  ">
+                <div class="col-md-5 ">
+                    <asp:Label ID="Label2" runat="server" Font-Size="14pt" Text="New file name"></asp:Label>
+                    <asp:TextBox CssClass=" col-md-6 form-control" ID="TextBox2" runat="server" CausesValidation="True"></asp:TextBox>
+                </div>
+                <div class="col-md-5">
+                    <asp:Label ID="Label4" runat="server" Font-Size="14pt" Text="Value"></asp:Label>
+                    <asp:TextBox CssClass="form-control" ID="TextBox3" runat="server" OnTextChanged="TextBox3_TextChanged"></asp:TextBox>
+
+                </div>
+                <asp:Button CssClass="btn btn-primary col-md-2 mt-3 " Style="height: 50px;" ID="Button4" runat="server" OnClick="Button4_Click" Text="Update" ViewStateMode="Enabled" />
+
+
+                <div class=" col-md-5 mt-3">
+
+                    <asp:Label ID="Label5" runat="server" Font-Size="14pt" Text="Select File Format "></asp:Label>
+
+                    <asp:DropDownList class="btn btn-outline-secondary dropdown-toggle" ID="DropDownList1" runat="server" DataSourceID="XmlDataSource1" DataTextField="text" DataValueField="value" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True">
+                    </asp:DropDownList>
+                      <asp:Button CssClass="btn btn-primary " ID="Button5" runat="server" OnClick="Button5_Click" Text="Add a new Format" ViewStateMode="Enabled" />
+
+
+                </div>
+                <div class="col-md-7  mt-3">
+                    <asp:FileUpload CssClass="form-control" ID="FileUpload1" runat="server" Visible="true" />
+
+                </div>
+
+
+            </div>
+
+            <asp:XmlDataSource ID="XmlDataSource1" runat="server" DataFile="~/FileFormatData/FileFormat.xml" OnTransforming="XmlDataSource1_Transforming"></asp:XmlDataSource>
+
+            <br />
+
+
+            <asp:Button CssClass="btn btn-primary col-md-2 mt-3 mx-4 " ID="Button1" runat="server" OnClick="Button1_Click" Text="Upload" />
+            <asp:Button CssClass="btn btn-primary col-md-2 mt-3 " ID="Button2" runat="server" OnClick="Button2_Click" Text="Dwonload" Enabled="False" />
+
+            <br />
+
+            <br />
+
+            <asp:GridView ID="GridView1" runat="server" Height="16px" Width="369px" CellPadding="4" ForeColor="#333333" GridLines="None" Style="margin-bottom: 0px">
+
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#7C6F57" />
+                <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#E3EAEB" />
+                <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                <SortedAscendingHeaderStyle BackColor="#246B61" />
+                <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                <SortedDescendingHeaderStyle BackColor="#15524A" />
+            </asp:GridView>
+        </div>
+
+        <%--<div id=" alert1">
+            <div class="alert alert-danger mt-3 ms-3 w-25 alert-dismissible fade show" role="alert">
+                Feel your all details
+             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+
+        </div>--%>
+    </form>
+    <script type="text/javascript">
+        setTimeout(function () {
+
+            // Closing the alert
+            $('#alert').alert('close');
+        }, 5000);
+    </script>
+</body>
+</html>
---
+++ scrubberTool/WebForm1.aspx.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace scrubberTool
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            TextBox3.Text = Convert.ToString(DropDownList1.SelectedItem.Value);//.DataBind();
+            TextBox2.Text = Convert.ToString(DropDownList1.SelectedItem.Text);//.DataBind();
+            Button4.Enabled = false;
+            TextBox2.Enabled = false;
+            TextBox3.Enabled = false;
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string path = Path.GetFileName(FileUpload1.FileName);
+                path = path.Replace(" ", "");
+                FileUpload1.SaveAs(Server.MapPath("~/ExcelFile/") + path);
+                String ExcelPath = Server.MapPath("~/ExcelFile/") + path;
+                OleDbConnection mycon = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + ExcelPath + "; Extended Properties=Excel 8.0; Persist Security Info = False");
+
+                mycon.Open();
+                OleDbCommand cmd = new OleDbCommand("select * from [Sheet1$]", mycon);
+                OleDbDataAdapter da = new OleDbDataAdapter();
+                da.SelectCommand = cmd;
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds.Tables[0];
+                GridView1.DataBind();
+                mycon.Close();
+                Button2.Enabled = true;
+
+            }
+            catch (Exception)
+            {
+                /*Response.Write("<div class=\"alert alert-danger mt-3 ms-3 w-25  alert-dismissible fade show\" role=\"alert\">\r\n  " +
+                         "  \r\n  " +
+                         "<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\">" +
+                         "</button>\r\n</div>");*/
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                string str = TextBox3.Text;
+                string[] spearator = { "," };
+                string[] strArray = str.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+                int[] intArray = Array.ConvertAll(strArray, s => int.Parse(s));
+
+
+                string textfile = string.Empty;
+                /*  foreach (TableCell tcell in GridView1.HeaderRow.Cells)
+                  {
+                      textfile += tcell.Text + "\t\t";
+                  }*/
+                // textfile += "\r\n";
+
+                foreach (GridViewRow row in GridView1.Rows)
+                {
+                    int i = 0;
+
+                    while (i < intArray.Length)
+
+
+                    {
+                        foreach (TableCell rowcell in row.Cells)
+                        {
+
+                            string ttt = rowcell.Text;
+
+                            string ass = ttt.Replace("&nbsp;", "").PadRight(intArray[i]);
+                            //textfile += ttt.PadRight(50);
+
+                            textfile += ass.Substring(0, intArray[i]) + " ";
+                            i++;
+
+
+                        }
+
+                    }
+                    textfile += "\r\n";
+                }
+                Response.Clear();
+                Response.Buffer = true;
+                Response.AddHeader("content-disposition ", "attachment;filename= text.txt");
+                Response.Charset = "";
+                Response.ContentType = "application/text";
+                Response.Output.Write(textfile);
+                Response.Flush();
+                Response.End();
+            }
+            catch (Exception)
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "alertMessage", "alertMessage();", true);
+                // Response.Write(ex);
+            }
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TextBox2.Text != String.Empty && TextBox3.Text != string.Empty)
+                {
+                    string filename = Server.MapPath("~/FileFormatData/FileFormat.xml");
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(filename);
+                    XmlNode RootNode = doc.SelectSingleNode("ListItems");
+                    // XmlNode node = doc.CreateNode(XmlNodeType.Element, " ListItem", null);
+                    XmlNode ListItem = RootNode.AppendChild(doc.CreateNode(XmlNodeType.Element, "ListItem", null));
+
+                    XmlAttribute _text = doc.CreateAttribute("text");
+                    _text.Value = TextBox2.Text;
+                    XmlAttribute _value = doc.CreateAttribute("value");
+
+                    _value.Value = TextBox3.Text;
+
+                    ListItem.Attributes.Append(_text);
+                    ListItem.Attributes.Append(_value);
+
+                    doc.DocumentElement.AppendChild(ListItem);
+
+                    doc.Save(filename);
+                    //  doc.DataBind(filename);
+                    // doc.BaseURI = Server.MapPath("~/FileFormatData/FileFormat.xml");
+                    TextBox3.Text = Convert.ToString(DropDownList1.SelectedItem.Value);
+                    TextBox2.Text = Convert.ToString(DropDownList1.SelectedItem.Text);
+                    Response.Write("<div class=\"alert alert-success  mt-3 ms-3 w-25  alert-dismissible fade show\" role=\"alert\">\r\n  " +
+                         "  New format is save\r\n  " +
+                         "<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\">" +
+                         "</button>\r\n</div>");
+                }
+                else
+                {
+                    Response.Write("<div class=\"alert alert-danger mt-3 ms-3 w-25  alert-dismissible fade show\" role=\"alert\">\r\n  " +
+                        "  Feel your all details\r\n  " +
+                        "<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\">" +
+                        "</button>\r\n</div>");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                //  Response.Write(ex);
+            }
+
+        }
+
+
+        protected void XmlDataSource1_Transforming(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            Button4.Enabled = true;
+            TextBox2.Enabled = true;
+            TextBox3.Enabled = true;
+            TextBox2.Text = string.Empty;
+            TextBox3.Text = string.Empty;
+            TextBox2.Focus();
+        }
+
+
+        public void DbData()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            using (SqlCommand cmd = con.CreateCommand())
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet dsLoadData = new DataSet();
+                con.Close();
+
+            }
+        }
+    }
+}
